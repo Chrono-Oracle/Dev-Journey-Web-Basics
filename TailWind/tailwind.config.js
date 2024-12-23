9c3853ea306ad1413c2eec32f4a3c3203e26bbf8
ChronoOracle <cryptonicdaniels@gmail.com>
2024-12-23 11:08:42 +0100

    Updated

--- TailWind/tailwind.config.js
+++ TailWind/tailwind.config.js
@@ -2,7 +2,39 @@
 module.exports = {
   content: ['./src/**/*.{html,js}'],
   theme: {
-    extend: {},
+    extend: {
+      scale: {
+        '1': '0.1',
+        '2': '0.2',
+        '3': '0.3',
+        '4': '0.4',
+        '5': '0.5',
+      },
+
+      gridTemplateColumns: {
+        //6 Column Grid
+        '6': 'repeat(6, minmax(0, 10em))',
+      },
+
+      //buttons
+      backgroundColor: {
+        'buttons': '#068465',
+      },
+
+      borderRadius: {
+        'universal': '6px',
+        'card': '12px',
+      },
+
+      //BackgroundImages
+      backgroundImage: {
+        'card-1': "url('/src/assets/images/card1.jpg')",
+        'card-2': "url('/src/assets/images/card2.jpg')",
+        'card-3': "url('/src/assets/images/card3.jpg')",
+        'card-4': "url('/src/assets/images/card4.jpg')",
+      }
+
+    },
   },
   plugins: [],
 }
